096102a36f595e10ae972aa71f6c63c445563275
SirYuriy <123596690+SirYuriy@users.noreply.github.com>
2025-05-10 18:00:51 +0300

    Initial commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Reiwa on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
